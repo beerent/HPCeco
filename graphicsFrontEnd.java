a9d4ac6fa1076c57e97ca7d2eec42a13c3ec10b5
Brent Ryczak <brentryczak@gmail.com>
2013-11-12 11:56:51 -0500

    adding a front end to the graphics portal for my specific client.

---
+++ graphicsFrontEnd.java
@@ -0,0 +1,11 @@
+public class graphicsFrontEnd{
+	public static void main(String [] args){
+		javaGraphicsPortal jgp = new javaGraphicsPortal("Brents World", 500, 500);
+		jgp.startPortal();
+	}
+
+	public static void sendMovements(){
+		
+	}
+
+}
